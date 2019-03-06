e7617f92d6e1af07d3b9342070b47aa59983bcde
daniyalv1851 <39700893+daniyalv1851@users.noreply.github.com>
2019-03-06 11:56:31 +0000

    sdsd

---
+++ 6.3.19/Assets/Scripts/Enemy.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float timeChange = 0;
+    private float MovementX;
+    private float MovementY;
+
+
+
+    public float moveSpeed = 0.2f;
+    public Rigidbody2D rb;
+
+    public float ChaseRadius;
+    public float AttackRadius;
+    public Animator animator;
+    public GameObject target;
+
+    public int health;
+    private int damagedlt;
+
+    public bool Moving;
+
+    Vector3 movement;
+    Vector3 temp;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+          
+        CheckDistance();
+        
+
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("AttackHB"))
+        {
+            damagedlt = collision.GetComponentInParent<Attack>().damage;
+            health -= damagedlt;
+            if (health <= 0)
+            {
+                gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (Time.time >= timeChange)
+        {
+            rb.velocity = new Vector2(0, 0);
+            MovementX = Random.Range(1, -2);
+            MovementY = Random.Range(1, -2);
+            timeChange = Time.time + 2;
+            movement = new Vector3(MovementX, MovementY, 0f);
+
+
+            rb.velocity += new Vector2(MovementX, MovementY) * moveSpeed;
+        }
+        
+    }
+
+
+    private void Animations()
+    {
+        animator.SetFloat("ZomX", rb.velocity.x);
+        animator.SetFloat("ZomY", rb.velocity.y);
+        animator.SetFloat("ZomMagnitude", rb.velocity.magnitude);
+
+    }
+    private void CheckDistance()
+    {
+        if(Vector3.Distance(target.transform.position, transform.position) <= ChaseRadius && Vector3.Distance(target.transform.position, transform.position) > AttackRadius)
+        {
+            Moving = target.GetComponent<Attack>().knocked;
+
+            if(Moving == false)
+            {
+                rb.velocity = new Vector2(0f, 0f);
+            }
+            temp = target.transform.position - transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+            animator.SetFloat("ZomX", temp.x);
+            animator.SetFloat("ZomY", temp.y);
+            animator.SetFloat("ZomMagnitude", temp.magnitude);
+
+        }
+        
+        else
+        {
+            Move();
+            Animations();
+        }
+   
+
+    }
+}
---
+++ 6.3.19/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject crossHair;
+    public Rigidbody2D rb;
+    public Vector2 maxaim;
+    public Vector2 minaim;
+
+
+
+
+    Vector3 movement;
+    Vector3 aim;
+    Vector2 cross;
+    
+
+
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Inputs();
+        Animations();
+        Move();
+        Shooting();
+        
+
+    }
+
+    private void Inputs()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal") * 2, Input.GetAxisRaw("Vertical") * 2, 0f);
+        Vector3 mouseMovement = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
+        aim = aim + mouseMovement;
+        aim.x = Mathf.Clamp(aim.x, minaim.x, maxaim.x);
+        aim.y = Mathf.Clamp(aim.y, minaim.x, maxaim.y);
+
+
+    }
+
+    private void Animations()
+    {
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+        animator.SetFloat("CrossX", cross.x);
+        animator.SetFloat("CrossY", cross.y);
+        animator.SetFloat("CrossMagnitude", cross.magnitude);
+        animator.SetBool("Aiming", Input.GetButtonDown("Fire1"));
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Collided");
+            rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
+            Animations();
+        }
+    }
+
+    private void Move()
+    {
+        // transform.position = transform.position + movement * Time.deltaTime;
+        rb.velocity = new Vector2(movement.x, movement.y);
+    }
+
+    private void Shooting()
+    {
+        crossHair.transform.position = aim;
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            cross = new Vector2(aim.x - transform.position.x, aim.y - transform.position.y);
+        }
+
+
+    }
+}
+
